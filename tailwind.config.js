8f621720773de4944163bab9ef0d527eca1eb1b9
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-12-25 06:56:07 -0300

    0601 Tailwind Config 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        verde: {
+          200: '#acef75',
+          300: '#91EE77',
+          900: '#16281F',
+          950: '#0F1C15',
+        },
+        green: {
+          // 300: '#91EE77',
+        },
+      },
+      spacing: {
+        22: '5.5rem',
+      },
+      fontSize: {
+        '2xs': '.5rem',
+        xl: ['1.25rem', { lineHeight: '1.5rem' }],
+      },
+      screens: {
+        xs: '380px',
+      },
+    },
   },
   plugins: [],
 };
