074b7cd10c90e82e6b246e8959a70c30814ee6e5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-12-25 07:04:30 -0300

    0601 Tailwind Config 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,51 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {
-      colors: {
-        verde: {
-          200: '#acef75',
-          300: '#91EE77',
-          900: '#16281F',
-          950: '#0F1C15',
-        },
-        green: {
-          // 300: '#91EE77',
-        },
-      },
-      spacing: {
-        22: '5.5rem',
-      },
-      fontSize: {
-        '2xs': '.5rem',
-        xl: ['1.25rem', { lineHeight: '1.5rem' }],
-      },
-      screens: {
-        xs: '380px',
-      },
-    },
+    colors: ({ colors }) => ({
+      lime: colors.lime,
+      white: 'white',
+    }),
+    // colors: {
+    //   lime: {
+    //     200: '#acef75',
+    //     300: '#91EE77',
+    //     900: '#16281F',
+    //     950: '#0F1C15',
+    //   },
+    //   white: 'white',
+    // },
+    // extend: {
+    //   colors: {
+    //     verde: {
+    //       200: '#acef75',
+    //       300: '#91EE77',
+    //       900: '#16281F',
+    //       950: '#0F1C15',
+    //     },
+    //     green: {
+    //       // 300: '#91EE77',
+    //     },
+    //   },
+    //   spacing: {
+    //     22: '5.5rem',
+    //   },
+    //   fontSize: {
+    //     '2xs': '.5rem',
+    //     xl: ['1.25rem', { lineHeight: '1.5rem' }],
+    //   },
+    //   screens: {
+    //     xs: '380px',
+    //   },
+    //   keyframes: {
+    //     slideIn: {
+    //       '0%': { opacity: 0, transform: 'translateX(-20px)' },
+    //       '100%': { opacity: 1, transform: 'translateX(0)' },
+    //     },
+    //   },
+    //   animation: {
+    //     slideIn: 'slideIn .3s ease-in-out forwards',
+    //   },
+    // },
   },
   plugins: [],
 };
