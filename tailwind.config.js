adc552d1acf939e6098f08a170df031f0db07853
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-12-27 06:52:04 -0300

    Projeto Final - 0802 Tailwind Configuração

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extends: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '.5rem',
+        sm: '1rem',
+      },
+    },
+    colors: {},
+    extend: {
+      colors: {
+        verde: {
+          200: '#ACEF75',
+          300: '#91EE77',
+          400: '#17E880',
+          700: '#2E482C',
+          800: '#16281F',
+          900: '#0F1C15',
+          950: '#030504',
+        },
+      },
+    },
   },
   plugins: [],
 };
